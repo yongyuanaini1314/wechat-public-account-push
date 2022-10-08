894ce5bdbc2380ad88993aca1fa47eb7f074acf0
yongyuanaini1314 <115344019+yongyuanaini1314@users.noreply.github.com>
2022-10-08 17:51:15 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '刘子炜',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'wx152292459c682435',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'KWLdC6PmVrwlcHFzP_1qrvJBZgs__l143rhdWAbfyqE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
